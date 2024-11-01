842baaa80cdcc6bfde2ce31f7950c9474ac89692
Ian Obando <kev6265@gmail.com>
2024-11-01 10:51:04 +0100

    changes

---
+++ week1/scripts/getdates.js
@@ -0,0 +1,16 @@
+/* 
+  Multi-line
+  comment
+*/
+
+// single line or remaining line comment
+
+// String literals
+let message = "Hello World!";
+/* OK to use double " or single ' quotes or backticks `.
+   You may not use decorative quotes.
+*/
+
+// output to screen
+document.getElementById("output").textContent =
+  "Welcome to Web Programming using JavaScript!";
